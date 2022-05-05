3ed6096e0ab7015214c0acbedbf3b5ed953e654a
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-05 12:56:38 -0700

    auth page and sign in form added

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
 
 export default function Auth() {
+  const { login } = useUser();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const location = useLocation();
+  const history = useHistory();
+
+  const handleFormSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      await login(email, password);
+
+      const url = location.state.origin ? location.state.origin.pathname : '/';
+      history.replace(url);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
-    <div>Auth</div>
-  )
+    <>
+      <h2>Auth Page</h2>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="password"
+        />
+        <button type="submit">sign in</button>
+        <p>{error}</p>
+      </form>
+    </>
+  );
 }
