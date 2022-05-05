49d76bed6583f2224cad9f0e7f36d6fd441c500a
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-05 11:54:33 -0700

    added private route to Guestbook.jsx in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Route, Switch } from 'react-router-dom';
 import Auth from './views/Auth/Auth';
 import Home from './views/Home/Home';
-
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Guestbook from './views/Users/Guestbook';
 
 export default function App() {
   return (
     <Switch>
-      <Route path='/login'>
+      <Route path="/login">
         <Auth />
       </Route>
-      <Route path='/'>
+      <PrivateRoute path="/guestbook">
+        <Guestbook />
+      </PrivateRoute>
+      <Route path="/">
         <Home />
       </Route>
     </Switch>
