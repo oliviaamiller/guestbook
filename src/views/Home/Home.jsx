fa4272715e8eeabe787e37bce99ec0c9eae74dca
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-05 12:58:26 -0700

    added home page with link to guestbook

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <h2>Home Page</h2>
+      <Link to="/guestbook">View Guestbook</Link>
+    </>
+  );
 }
