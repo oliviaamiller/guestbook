1f1d3c350c06d45db30ff05ebcca596531c3d992
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-06 14:33:41 -0700

    user email showing in guestbook heading

--- src/views/Users/Guestbook.jsx
+++ src/views/Users/Guestbook.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { getEntries } from '../../services/entries';
 
 export default function Guestbook() {
-  const { logout } = useUser();
+  const { logout, user } = useUser();
   const [entries, setEntries] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -20,6 +20,16 @@ export default function Guestbook() {
   return (
     <>
       <h2>Guestbook</h2>
+      <p>Hello {user.email}, you logged in successfully! here are your entries:</p>
+      {/* {loading ? (
+        <p>loading...</p>
+      ) : (
+        <ul>
+          {entries.map((entry) => (
+            <li key={entry.id}>{entry.content}</li>
+          ))}
+        </ul>
+      )} */}
       <button onClick={logout}>log out</button>
     </>
   );
