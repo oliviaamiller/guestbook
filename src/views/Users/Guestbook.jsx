bb10ab501899a8c1fda7fd00739debaf70efed4d
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-06 14:21:01 -0700

    added useEffect to fetch entries on load in Guestbook.jsx

--- src/views/Users/Guestbook.jsx
+++ src/views/Users/Guestbook.jsx
@@ -1,7 +1,21 @@
 import { useUser } from '../../context/UserContext';
+import { useEffect, useState } from 'react';
+import { getEntries } from '../../services/entries';
 
 export default function Guestbook() {
   const { logout } = useUser();
+  const [entries, setEntries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEntries = async () => {
+      const results = await getEntries();
+
+      setEntries(results);
+      setLoading(false);
+    };
+    fetchEntries();
+  }, []);
 
   return (
     <>
